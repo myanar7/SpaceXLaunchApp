affcbef64be7bc76f9956e8b1245cf447bf66867
Mustafa Yanar <mustafayanar0007@gmail.com>
2023-10-05 02:01:51 +0300

    initial commit

---
+++ spaceXTracker/Service.swift
@@ -0,0 +1,16 @@
+//
+//  Service.swift
+//  spaceXTracker
+//
+//  Created by Mustafa Yanar on 2.10.2023.
+//
+
+import Foundation
+
+class Service {
+    static let shared = Service()
+    
+    init() {
+        
+    }
+}
---
+++ spaceXTracker/spaceXTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  spaceXTrackerApp.swift
+//  spaceXTracker
+//
+//  Created by Mustafa Yanar on 2.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct spaceXTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
